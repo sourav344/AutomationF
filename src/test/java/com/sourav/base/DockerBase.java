777dd07f145e17aae0c62e2d02dd285251895475
Sourav Paul <sourav344@gmail.com>
2024-09-02 23:18:49 +0530

    final working CI

--- src/test/java/com/sourav/base/DockerBase.java
+++ src/test/java/com/sourav/base/DockerBase.java
@@ -7,28 +7,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DockerBase {
 
-	public WebDriver initializeBrowser(String browserName) throws MalformedURLException
-	{
-		
-		  if(browserName.equals("chrome")) {
-		        ChromeOptions options = new ChromeOptions();
-		        options.setAcceptInsecureCerts(true); // This bypasses SSL validation
-		        return new RemoteWebDriver(new URL("http://13.233.101.243:4444"), options);
-		    } else if(browserName.equals("firefox")) {
-		        FirefoxOptions options = new FirefoxOptions();
-		        options.setAcceptInsecureCerts(true); // This bypasses SSL validation
-		        return new RemoteWebDriver(new URL("http://13.233.101.243:4444"), options);
-		    }
-		    else if(browserName.equals("edge")) {
-		        EdgeOptions options = new EdgeOptions();
-		        options.setAcceptInsecureCerts(true); // This bypasses SSL validation
-		        return new RemoteWebDriver(new URL("http://13.233.101.243:4444"), options);
-		    }
-		  throw new IllegalArgumentException("Browser name not supported");
-	}
+    public WebDriver initializeBrowser(String browserName) throws MalformedURLException {
+        // URL for the Selenium Grid hub
+        URL gridUrl = new URL("http://13.233.101.243:4444/wd/hub");
+        
+        if(browserName.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.setAcceptInsecureCerts(true); // This bypasses SSL validation
+            return new RemoteWebDriver(gridUrl, options);
+        } else if(browserName.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            options.setAcceptInsecureCerts(true); // This bypasses SSL validation
+            return new RemoteWebDriver(gridUrl, options);
+        } else if(browserName.equalsIgnoreCase("edge")) {
+            EdgeOptions options = new EdgeOptions();
+            options.setAcceptInsecureCerts(true); // This bypasses SSL validation
+            return new RemoteWebDriver(gridUrl, options);
+        }
+        throw new IllegalArgumentException("Browser name not supported");
+    }
 }
